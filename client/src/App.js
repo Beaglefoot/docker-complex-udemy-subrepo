63df0b40a571de27997dcd10f026f762ef027eef
Beaglefoot <ch-e-st@yandex.ru>
2019-12-22 18:00:47 +0300

    Modify client to play with k8s redeployements

--- client/src/App.js
+++ client/src/App.js
@@ -4,24 +4,27 @@ import './App.css';
 import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
 import OtherPage from './OtherPage';
 import Fib from './Fib';
+import { ErrorBoundary } from './ErrorBoundary';
 
 class App extends Component {
   render() {
     return (
-      <Router>
-        <div className="App">
-          <header className="App-header">
-            <img src={logo} className="App-logo" alt="logo" />
-            <h1 className="App-title">Welcome to React</h1>
-            <Link to="/">Home</Link>
-            <Link to="/otherpage">Other Page</Link>
-          </header>
-          <div>
-            <Route exact path="/" component={Fib} />
-            <Route path="/otherpage" component={OtherPage} />
+      <ErrorBoundary>
+        <Router>
+          <div className="App">
+            <header className="App-header">
+              <img src={logo} className="App-logo" alt="logo" />
+              <h1 className="App-title">Fib Calculator v2</h1>
+              <Link to="/">Home</Link>
+              <Link to="/otherpage">Other Page</Link>
+            </header>
+            <div>
+              <Route exact path="/" component={Fib} />
+              <Route path="/otherpage" component={OtherPage} />
+            </div>
           </div>
-        </div>
-      </Router>
+        </Router>
+      </ErrorBoundary>
     );
   }
 }
